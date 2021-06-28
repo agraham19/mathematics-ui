d349937d7583a924dee122c23ff9d91a2aaf1d39
agraham1988 <35735033+agraham1988@users.noreply.github.com>
2021-06-28 15:02:11 +0100

    test

--- src/components/Sieve.jsx
+++ src/components/Sieve.jsx
@@ -1,9 +1,9 @@
-import {useState} from 'react';
-import axios from "axios";
-import SieveSteps from "./SieveSteps";
+import { useState } from 'react';
+import axios from 'axios';
+import SieveSteps from './SieveSteps';
 
 const Sieve = () => {
-    const [steps, setSteps] = useState([])
+    const [steps, setSteps] = useState([]);
     const [upperLimit, setValue] = useState(20);
 
     const handleSubmit = (upperLimit) => (e) => {
@@ -12,30 +12,34 @@ const Sieve = () => {
 
         console.log(`Submitting sieve request for: ${upperLimit}`);
 
-        axios.get(`http://localhost:8082/find_primes_up_to/${upperLimit}`).then(response => {
+        axios.get(`http://localhost:8082/find_primes_up_to/${upperLimit}`).then((response) => {
             setSteps(response.data.steps);
         });
     };
 
     return (
         <>
-            <div>Input value: {upperLimit}</div>
-            <SieveInput handleSubmit={handleSubmit}/>
-            <SieveSteps steps={steps} upperLimit={upperLimit}/>
-        </>
+            <div>
+            Input value:{upperLimit}
+          </div>
+            <SieveInput handleSubmit={handleSubmit} />
+            <SieveSteps steps={steps} upperLimit={upperLimit} />
+      </>
     );
-}
+};
 
-const SieveInput = ({handleSubmit}) => {
+const SieveInput = ({ handleSubmit }) => {
     const [upperLimit, setValue] = useState(20);
     const onChange = (event) => {
         setValue(parseInt(event.target.value));
     };
 
-    return (<>
-        <input value={upperLimit} onChange={onChange}/>
-        <button onClick={handleSubmit(upperLimit)}>Sieve!</button>
-    </>);
-}
+    return (
+      <>
+          <input value={upperLimit} onChange={onChange} />
+          <button onClick={handleSubmit(upperLimit)}>Sieve!</button>
+        </>
+    );
+};
 
 export default Sieve;
